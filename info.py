7e2eb2d8a74fb2db4a0e4905f6ba8fc3feb3a4db
Luiz Esquivel <62815987+luizesquivel05@users.noreply.github.com>
2023-02-03 00:01:11 -0300

    criando tabela da nba

--- info.py
+++ info.py
@@ -1,3 +1,13 @@
 times = [['Boston Celtics','Brooklyn Nets','New York Knicks','Philadelphia 76ers','Toronto Raptors','Chicago Bulls','Cleveland Cavaliers','Detroit Pistons','Indiana Pacers','Milwaukee Bucks','Atlanta Hawks','Charlotte Hornets','Miami Heat','Orlando Magic','Washington Wizards'],['Denver Nuggets','Minnesota Timberwolves','Portland Trail Blazers','Oklahoma City Thunder','Utah Jazz','Golden State Warriors','Los Angeles Clippers','Los Angeles Lakers','Phoenix Suns','Sacramento Kings','Dallas Mavericks','Houston Rockets','Memphis Grizzlies','New Orleans Pelicans','San Antonio Spurs']]
 CL = times[0]
 CO = times[1]
+print(times)
+cont = 0
+for i in range(0, 2):
+    for j in times[i]:
+        j = str(j)
+        print(len(j), cont)
+        cont += 1
+print(len(CL))
+print(len(times[1][1]))
+print(times[1][1])
--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import os
 
 print('Salve, salve caro usuário? Bem-vindo ao PROJETO TABELANBA.')
 while True:
-    print('O que deseja fazer hoje? Digite NBA para ler sobre NBA ou TBNA para ler sobre o projet e SAIR para sair do programa.')
+    print('O que deseja fazer hoje? Digite NBA para ler sobre NBA ou TBNA para ler sobre o projeto ou TABELANBA para ver a tabela geral da NBA ou SAIR para sair do programa.')
     verificar = str(input('Digite sua opção: '))
-    while verificar != "NBA" and verificar != "TNBA" and verificar != "SAIR":
+    while verificar != "NBA" and verificar != "TNBA" and verificar != "TABELANBA" and verificar != "SAIR":
         verificar = str(input('Digite uma opção válida: '))
     else:
         os.system('cls')
@@ -15,5 +15,8 @@ while True:
     elif verificar == "TNBA":
         os.system('cls')
         print(txt.sobreTABELANBA)
+    elif verificar == "TNBA":
+        os.system('cls')
+        print(txt.tabelaNBA)
     else:
         break
